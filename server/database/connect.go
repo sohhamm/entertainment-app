9671441d1c8b3f3b41f8c193822639ea6c52fc88
Soham Sarkar <soham.ss1@gmail.com>
2022-10-05 13:00:41 +0530

    db connection

--- server/database/connect.go
+++ server/database/connect.go
@@ -1,6 +1,52 @@
 package database
 
+import (
+	"log"
+	"os"
+
+	"github.com/sohhamm/entertainment-app/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var DB DbInstance
+
 func connectDB() {
 	var err error
+
+	db_url := config.Config("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(db_url), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Error connecting to the db 😞 \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("Connected to the database 🔥")
+
+	sqlDB, err := db.DB()
+
+	if err != nil {
+		log.Fatal("Connection Pooling failed")
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Running migrations .... 🚂")
+
+	db.AutoMigrate()
+
+	DB = DbInstance{Db: db}
 
 }
