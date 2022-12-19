08cdea01cfa8ba596c118f2c09dfe294a6901111
Soham Sarkar <soham.ss1@gmail.com>
2022-12-19 09:19:21 +0530

    solid router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import type {Component} from 'solid-js'
 import '../src/styles/global.css'
+import {Routes, Route} from '@solidjs/router'
+import Home from './pages/home/Home'
 
 const App: Component = () => {
-  return <div>webapp</div>
+  return (
+    <Routes>
+      <Route path='/' component={Home} />
+    </Routes>
+  )
 }
 
 export default App
