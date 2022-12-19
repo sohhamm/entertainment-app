f7a65b2d11344c03f2eb25d2880e5e29b23b1ed6
Soham Sarkar <soham.ss1@gmail.com>
2022-12-19 09:15:30 +0530

    frontend boilerplate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,8 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type {Component} from 'solid-js'
+import '../src/styles/global.css'
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+  return <div>webapp</div>
+}
 
-export default App;
+export default App
